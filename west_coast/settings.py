d3652903180b301fafb10d2740a8a889b195e702
blackcatten sweden <m_arnesson@hotmail.com>
2024-04-04 10:21:01 +0000

    Added allowed host

--- west_coast/settings.py
+++ west_coast/settings.py
@@ -19,7 +19,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.environ.join(BASE_DIR, 'templates')
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['west-coast-restaurant.herokuapp.com', 'localhost','8000-blackcatten-westcoastre-ogi93qrz2vt.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['west-coast-restaurant-901306ae347b.herokuapp.com', 'localhost','8000-blackcatten-westcoastre-ogi93qrz2vt.ws-eu110.gitpod.io']
 
 
 # Application definition

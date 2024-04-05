52b79da595694650a9549f7e1c30b0e909669ec9
blackcatten sweden <m_arnesson@hotmail.com>
2024-04-05 13:45:00 +0000

    Add models.py content to booking_system

--- booking_system/models.py
+++ booking_system/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Guest_list(models.Model):
+    guest = models.IntegerField(null=False, blank=False)
+
+    def __str__(self):
+        return str(self.guest)
+
+
+class Reservations(models.Model):
+
+    TIME_CHOICES = [
+        ("12 PM - 2 PM", "12 PM - 2 PM"),
+        ("1 PM - 3 PM", "1 PM - 3 PM"),
+        ("2 PM - 4 PM", "2 PM - 4 PM"),
+        ("3 PM - 5 PM", "3 PM - 5 PM"),
+        ("4 PM - 6 PM", "4 PM - 6 PM"),
+        ("5 PM - 7 PM", "5 PM - 7 PM"),
+        ("6 PM - 8 PM", "6 PM - 8 PM"),
+    ]
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reservation'
+    )
+    full_name = models.CharField(max_length=60, blank=False)
+    email = models.EmailField()
+    guest_list = models.ForeignKey(Guest_list, on_delete=models.CASCADE)
+    date = models.DateField(null=False, blank=False)
+    time = models.CharField(null=True, blank=False,
+                            choices=TIME_CHOICES, max_length=60)
+    image = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return str(self.date)
+

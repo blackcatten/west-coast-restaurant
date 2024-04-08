305bf6a431c5c94924d09c29813cc88b21e2fcd2
blackcatten sweden <m_arnesson@hotmail.com>
2024-04-08 06:40:54 +0000

    add reservationlist to view

--- booking_system/views.py
+++ booking_system/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Guest_list, Reservations
 
-# Create your views here.
+
+class ReservationList(generic.ListView):
+    model = Reservations
+    queryset = Reservations.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
+

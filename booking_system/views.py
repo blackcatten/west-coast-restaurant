556f364df4dd575ae25dd71c2617b78be12a149e
blackcatten sweden <m_arnesson@hotmail.com>
2024-04-09 08:24:25 +0000

    Add reservations form to view and add reservations_detail.html

--- booking_system/views.py
+++ booking_system/views.py
@@ -1,9 +1,48 @@
 from django.shortcuts import render
 from django.views import generic, View
 from .models import Guest_list, Reservations
+from .forms import ReservationsForm
 
 
 class ReservationList(generic.ListView):
     model = Reservations
     template_name = "index.html"
+
+
+class ReservationsDetail(View):
+
+    template_name = 'reservations_detail.html'
+
+    def get(self, request):
+
+        context = {
+                'reservations': reservations,
+            }
+
+        return render(
+            request,
+            "reservations_detail.html", context
+        )
+
+
+
+    def post(self, request):
+
+        form = ReservationsForm(request.POST)
+        
+        if reservations_form.is_valid():
+            reservations_form.instance.name = request.user.username
+            reservations = reservations_form.save(commit=False)
+            reservations.post = post
+            reservations.save()
+        else:
+            reservations_form = ReservationsForm()
+
+        return render(
+            request,
+            "reservation_detail.html",
+            {
+                "form": form,
+            },
+        )
 

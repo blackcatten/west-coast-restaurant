f85bc3e5f112d8aad39b9a833448ad3cc6fab36e
blackcatten sweden <m_arnesson@hotmail.com>
2024-04-18 19:45:05 +0000

    Add X_FRAME_OPTIONS = 'SAME_ORIGIN' to settings.py

--- west_coast/settings.py
+++ west_coast/settings.py
@@ -170,3 +170,4 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+X_FRAME_OPTIONS = 'SAME_ORIGIN'
